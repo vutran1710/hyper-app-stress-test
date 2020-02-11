db9a08d7059a0796c9509cc6034392d3cb6edab3
vutr <me@vutr.io>
2020-02-11 15:26:59 +0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-// #![deny(warnings)]
+#![deny(warnings)]
 
-// use std::sync::{
-//     atomic::{AtomicUsize, Ordering},
-//     Arc,
-// };
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
 
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Error, Response, Server};
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::init();
-
     let addr = ([127, 0, 0, 1], 3000).into();
 
     // For the most basic of state, we just share a counter, that increments
     // with each request, and we send its value back in the response.
-    // let counter = Arc::new(AtomicUsize::new(0));
-    let mut counter: u32 = 0;
+    let counter = Arc::new(AtomicUsize::new(0));
 
     // The closure inside `make_service_fn` is run for each connection,
     // creating a 'service' to handle requests for that specific connection.
@@ -28,7 +25,7 @@ async fn main() {
         //
         // Each connection could send multiple requests, so
         // the `Service` needs a clone to handle later requests.
-        // let counter = counter.clone();
+        let counter = counter.clone();
 
         async move {
             // This is the `Service` that will handle the connection.
@@ -37,14 +34,15 @@ async fn main() {
             Ok::<_, Error>(service_fn(move |_req| {
                 // Get the current count, and also increment by 1, in a single
                 // atomic operation.
-                counter += 1;
-                let resp = format!("Goodbye Hello World: {}", counter);
-                async move { Ok::<_, Error>(Response::new(Body::from(resp))) }
+                let count = counter.fetch_add(1, Ordering::AcqRel);
+                async move { Ok::<_, Error>(Response::new(Body::from(format!("Request #{}", count)))) }
             }))
         }
     });
 
     let server = Server::bind(&addr).serve(make_service);
+
+    println!("Listening on http://{}", addr);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
